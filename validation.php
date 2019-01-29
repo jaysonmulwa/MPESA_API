845f7d2b606a446a0f0426efb94ce355faf7d729
Jayson Mulwa <jayson.mulwa@gmail.com>
2019-01-29 11:16:22 +0300

    Add files via upload

---
+++ validation.php
@@ -0,0 +1,35 @@
+<?php
+
+
+header ("Content-Type:application/json");
+
+$response='{
+		"ResultCode"=0,
+
+		"ResultDesc"="Confirmation Received Succesfully",
+
+
+
+}';
+
+//0-accepted
+
+//DATA
+$mpesaResponse=file_get_contents('php://input');
+
+//LOG
+
+$logFile="mpesaResponse.txt";
+
+$jsonMpesaResponse=json_decode($mpesaResponse,true);
+
+//write to log file
+
+$log=fopen($logFile,"a");
+
+fwrite($log, $mpesaResponse);
+
+fclose($log);
+
+echo $response;
+?>
